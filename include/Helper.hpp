8c7075b66a79557f8b70d98ba87745c0ca168673
Allan Navarro <allan.nb.cr@gmail.com>
2018-09-02 20:04:19 -0600

    se cambia el nombre y namespace

--- include/Helper.hpp
+++ include/Helper.hpp
@@ -13,7 +13,7 @@
 
 namespace bmt=boost::math::tools; // for polynomial
 
-namespace hvins{
+namespace helper{
     // default template
     template<class T_Coeff,class T_Roots, class Enable = void, class Enable2 = void>
     class Helper {
@@ -21,7 +21,7 @@ namespace hvins{
         explicit Helper(const bmt::polynomial<T_Coeff> &poly) {
 
         }
-        void test(std::vector<T_Roots>  &roots){
+        void solve(std::vector<T_Roots> &roots){
 
         }
     };
@@ -36,17 +36,21 @@ namespace hvins{
         explicit Helper(const bmt::polynomial<T_Coeff> &poly) : mPoly(poly){
 
         }
-        void test(std::vector<T_Roots>  &roots){
+        void solve(std::vector<T_Roots> &roots){
             unsigned long degree = mPoly.degree();
             std::vector<T_Coeff> zeror(degree), zeroi(degree);
             Jenkins::RPoly<T_Coeff>().findRoots(mPoly.data(), static_cast<int>(degree), zeror, zeroi);
             for (unsigned long i = 0; i < degree; ++i) {
-                roots.push_back(zeror[i]);
+                if(std::abs(zeroi[i])>=std::numeric_limits<T_Coeff>::epsilon()){
+                    roots.push_back(std::numeric_limits<T_Coeff>::quiet_NaN());
+                }else {
+                    roots.push_back(zeror[i]);
+                }
             }
         }
     };
 
-    // specialization for complex roots
+    // specialization for complex roots with real coefficients
     template<class T_Coeff, class T_Roots>
     class Helper<T_Coeff,T_Roots,
             typename std::enable_if<(std::is_floating_point<T_Coeff>::value)>::type,
@@ -56,7 +60,7 @@ namespace hvins{
         explicit Helper(const bmt::polynomial<T_Coeff> &poly) : mPoly(poly){
 
         }
-        void test(std::vector<T_Roots>  &roots){
+        void solve(std::vector<T_Roots> &roots){
             unsigned long degree = mPoly.degree();
             std::vector<T_Coeff> zeror(degree), zeroi(degree);
             Jenkins::RPoly<T_Coeff>().findRoots(mPoly.data(), static_cast<int>(degree), zeror, zeroi);
